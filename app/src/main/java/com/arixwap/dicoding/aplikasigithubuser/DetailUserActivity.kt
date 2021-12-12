e0ac27619e6dc53fee057f91c7be99938ee3fdf6
Arix Wap <arix.wap@gmail.com>
2021-12-12 22:37:00 +0800

    display user detail count follower and following

--- app/src/main/java/com/arixwap/dicoding/aplikasigithubuser/DetailUserActivity.kt
+++ app/src/main/java/com/arixwap/dicoding/aplikasigithubuser/DetailUserActivity.kt
@@ -1,9 +1,9 @@
 package com.arixwap.dicoding.aplikasigithubuser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.arixwap.dicoding.aplikasigithubuser.databinding.ActivityDetailUserBinding
 import com.bumptech.glide.Glide
@@ -12,6 +12,9 @@ import com.google.android.material.tabs.TabLayoutMediator
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.DecimalFormat
+import kotlin.math.ln
+import kotlin.math.pow
 
 class DetailUserActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailUserBinding
@@ -28,18 +31,26 @@ class DetailUserActivity : AppCompatActivity() {
 
     private fun showUserDetail() {
         showLoading(true)
+
         val username = intent.getStringExtra(EXTRA_USERNAME)
+        var followers = 0
+        var following = 0
+
         if (username != null) {
             val request = GithubApiConfig.getApiService().getUserDetail(username)
             request.enqueue(object : Callback<UserDetailResponse> {
                 override fun onResponse(call: Call<UserDetailResponse>, response: Response<UserDetailResponse>) {
                     if (response.isSuccessful) {
                         val responseBody = response.body()
-                        val username = responseBody?.login
-                        binding.nameDetailUser.text = username
+
+                        // Display information textview
                         binding.usernameDetailUser.text = username
                         if (responseBody?.name != null) {
+                            // Display user full name
                             binding.nameDetailUser.text = responseBody.name
+                        } else {
+                            // Use username as user full name
+                            binding.nameDetailUser.text = username
                         }
                         if (responseBody?.location != null) {
                             binding.locationDetailUser.text = responseBody.location
@@ -50,13 +61,23 @@ class DetailUserActivity : AppCompatActivity() {
                             binding.companyDetailUser.visibility = View.VISIBLE
                         }
                         if (responseBody?.publicRepos != null) {
-                            binding.repositoryDetailUser.text = getString(R.string.num_repository, responseBody.publicRepos)
+                            binding.repositoryDetailUser.text = getString(R.string.num_repository, prettyNumber(responseBody.publicRepos))
                             binding.repositoryDetailUser.visibility = View.VISIBLE
                         }
+
+                        // Display user avatar
                         Glide.with(binding.root)
                             .load(responseBody?.avatarUrl)
                             .circleCrop()
                             .into(binding.imgDetailUser)
+
+                        // Get follower & following count
+                        if (responseBody?.followers != null) {
+                            followers = responseBody.followers
+                        }
+                        if (responseBody?.following != null) {
+                            following = responseBody.following
+                        }
                     } else {
                         Toast.makeText(this@DetailUserActivity, response.toString(), Toast.LENGTH_SHORT).show()
                     }
@@ -67,7 +88,11 @@ class DetailUserActivity : AppCompatActivity() {
                     viewPager.adapter = sectionsPagerAdapter
                     val tabs: TabLayout = binding.tabsDetailUser
                     TabLayoutMediator(tabs, viewPager) { tab, position ->
-                        tab.text = TAB_TITLES[position]
+                        tab.text = when(position) {
+                            0 -> getString(R.string.num_follower, prettyNumber(followers))
+                            1 -> getString(R.string.num_following, prettyNumber(following))
+                            else -> null
+                        }
                     }.attach()
                     supportActionBar?.elevation = 0f
 
@@ -99,11 +124,16 @@ class DetailUserActivity : AppCompatActivity() {
         }
     }
 
+    private fun prettyNumber(count: Int): String {
+        if (count < 1000) return "" + count
+        val exp = (ln(count.toDouble()) / ln(1000.0)).toInt()
+        val format = DecimalFormat("0.#")
+        val value: String = format.format(count / 1000.0.pow(exp.toDouble()))
+        return String.format("%s%c", value, "kMBTPE"[exp - 1])
+    }
+
+
     companion object {
         const val EXTRA_USERNAME = "extra_username"
-        private val TAB_TITLES = arrayListOf(
-            "Followers",
-            "Following"
-        )
     }
 }
